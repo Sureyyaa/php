c594878b48e7b0dfd08ccada805b9f4919ffa60a
Sureyya <sureyya.s@code.edu.az>
2016-08-20 12:33:50 +0400

    img gallery unlink

--- images gallery/delete.php
+++ images gallery/delete.php
@@ -5,13 +5,20 @@
 
 	$id = $_GET["id"];
 
+	$sekil_adi = "SELECT * FROM  gallery WHERE  id='$id'";
+
 	$sql = "DELETE FROM gallery WHERE id='$id'";
 
-	$query = mysqli_query($con, $sql);
+	$query = mysqli_query($con, $sekil_adi);
 
+	$query2 = mysqli_query($con, $sql);
+
+	$row=mysqli_fetch_assoc($query);
+  	
+	unlink("images/".$row['img_name']);
+	
 	header('Location: index.php');
 
 
 
 ?>
-
